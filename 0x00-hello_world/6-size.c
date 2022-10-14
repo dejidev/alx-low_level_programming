5ac830f9951d184a1c8cd61977ae3eff00ab52fe
Ayodeji Emmanuel Adetayo <adetayoayodeji7@gmail.com>
2022-10-13 19:23:00 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0 Always
+ */
+int main(void)
+{
+	printf("Size of char: %i bytes(s)\n", sizeof(char));
+	printf("Size of int: %i byte(s)\n", sizeof(int));
+	printf("Size of long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %i bytes(s)\n", sizeof(long int));
+	printf("Size of float: %i byte(s)\n", sizeof(float));
+}
